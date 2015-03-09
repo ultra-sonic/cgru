3f1835429007f15f2b0a5428574484eeb9086d05
Oliver Markowski <oliver@fullblownimages.com>
2015-03-10 00:15:32 +0100

    removing CMakeCache before building for another OS

--- afanasy/src/project.cmake/build.sh
+++ afanasy/src/project.cmake/build.sh
@@ -72,11 +72,7 @@ case ${DISTRIBUTIVE} in
     CentOS)
         export ADD_CMAKE_MODULE_PATH="$PWD"
         export AF_EXTRA_LIBS="pthread"
-<<<<<<< HEAD
 	;;
-=======
-        ;;
->>>>>>> 22c307c07562bca34d5a9e8818ea42cad384197f
     RedHat)
         export AF_ADD_LFLAGS="$AF_ADD_LFLAGS -lpthread -ldl"
         ;;
--- afanasy/src/project.cmake/build_ostype.sh
+++ afanasy/src/project.cmake/build_ostype.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+rm CMakeCache.txt -f
 export AF_OSTYPE=$OSTYPE
 
 [ -z "$AF_OSTYPE" ] && export AF_OSTYPE=`uname -s`
