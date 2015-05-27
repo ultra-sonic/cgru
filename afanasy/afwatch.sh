178e7648f62ab6c3b26f6550829598067b1f2b9e
Oliver Markowski <oliver@fullblownimages.com>
2015-05-27 10:44:43 +0300

    made sourcing of setup.sh relative to the scriptpath

--- afanasy/afwatch.sh
+++ afanasy/afwatch.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-pushd /NAS/renderfarm/cgru/afanasy
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+pushd $DIR
 source setup.sh
 GCC_VERSION=`gcc -dumpversion`
 ./bin_${GCC_VERSION}/afwatch
