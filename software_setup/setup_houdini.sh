67cbe2993641f272c4786cd91ff271234b4293e9
Roderick <roderick.friedrich@bighugfx.com>
2015-05-29 17:54:49 +0200

    software setup adjusted

--- software_setup/setup_houdini.sh
+++ software_setup/setup_houdini.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Source general for all soft directives:
-source "$CGRU_LOCATION/software_setup/setup__all.sh"
+# source "$CGRU_LOCATION/software_setup/setup__all.sh"
 
 # Search directory where Houdini installed:
 HOUDINI_INSTALL_DIR="/opt"
@@ -13,23 +13,29 @@ HOUDINI_INSTALL_DIR="/opt"
 export HOUDINI_LOCATION="${HOUDINI_INSTALL_DIR}/hfs${HOUDINI_VERSION}"
 
 #Override houdini location based on locate_houdini.sh
-locate_houdini="$CGRU_LOCATION/software_setup/locate_houdini.sh"
-if [ -f $locate_houdini ]; then
-	source $locate_houdini
-	HOUDINI_LOCATION="$APP_DIR"
-fi
+# locate_houdini="$CGRU_LOCATION/software_setup/locate_houdini.sh"
+# if [ -f $locate_houdini ]; then
+#	source $locate_houdini
+#	HOUDINI_LOCATION="$APP_DIR"
+# fi
 
 # Check Houdini location:
 if [ -z "$HOUDINI_LOCATION" ]; then
 	echo "Can't find houdini in '$HOUDINI_INSTALL_DIR'"
 	exit 1
 fi
-echo "Houdni location = '$HOUDINI_LOCATION'"
+echo "Houdini location = '$HOUDINI_LOCATION'"
+
+export HSITE="/NAS/development/production/houdini"
 
 # Source Houdini setup shell script:
 pushd $HOUDINI_LOCATION >> /dev/null
 source houdini_setup_bash
 popd $pwd >> /dev/null
+
+# Houdini Lic Server
+# export SESI_LMHOST="mcs"
+# hserver -S $SESI_LMHOST
 
 # Setup CGRU houdini scripts location:
 export HOUDINI_CGRU_PATH=$CGRU_LOCATION/plugins/houdini
@@ -38,14 +44,14 @@ export HOUDINI_CGRU_PATH=$CGRU_LOCATION/plugins/houdini
 export PYTHONPATH=$HOUDINI_CGRU_PATH:$PYTHONPATH
 
 # Define OTL scan path:
-HOUDINI_CGRU_OTLSCAN_PATH=$HIH/otls:$HOUDINI_CGRU_PATH:$HH/otls
+HOUDINI_CGRU_OTLSCAN_PATH=$HIH/otls:$HOUDINI_CGRU_PATH
 
 # Create or add to exist OTL scan path:
-if [ "$HOUDINI_OTLSCAN_PATH" != "" ]; then
-	export HOUDINI_OTLSCAN_PATH="${HOUDINI_CGRU_OTLSCAN_PATH}:${HOUDINI_OTLSCAN_PATH}"
-else
-	export HOUDINI_OTLSCAN_PATH=$HOUDINI_CGRU_OTLSCAN_PATH
-fi
+# if [ "$HOUDINI_OTLSCAN_PATH" != "" ]; then
+#	export HOUDINI_OTLSCAN_PATH="${HOUDINI_CGRU_OTLSCAN_PATH}:${HOUDINI_OTLSCAN_PATH}"
+# else
+export HOUDINI_OTLSCAN_PATH=${HOUDINI_OTLSCAN_PATH}:@/otls:${HOUDINI_CGRU_OTLSCAN_PATH}
+# fi
 
 export APP_DIR="$HOUDINI_LOCATION"
 export APP_EXE="houdini"
